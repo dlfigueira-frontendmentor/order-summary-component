a0e5489d310b84aa838fe56056b8d5d6eadfe00d
Daniel Figueira <daniel.figueira@relexsolutions.com>
2023-01-22 16:09:16 +0000

    Added responsive background Added font family Replaced favicon

--- src/App.vue
+++ src/App.vue
@@ -16,16 +16,35 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-}
+@import "@/styles/_variables.scss";
 
 * {
   box-sizing: border-box;
   margin: 0;
 }
+
+#app {
+  background-image: url("@/assets/pattern-background-mobile.svg");
+  background-repeat: no-repeat;
+  background-color: $pale_blue;
+  background-size: 100% auto;
+  font-family: "Red Hat Display", serif;
+  font-size: 1rem;
+}
+
+@media only screen and (min-width: 1440px) {
+  #app {
+    background-image: url("@/assets/pattern-background-desktop.svg");
+  }
+}
 </style>
 
 <style lang="scss" scoped>
 .main-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
 }
 </style>
